5117b9bd95d551d5d124fa15dbba101ad50ae476
Vercingetorix78 <maxboykomax@gmail.com>
2015-07-25 17:24:35 -0700

    Tweaks

--- run_analysis.R
+++ run_analysis.R
@@ -22,10 +22,13 @@ colnames(testSet) <- featureNames$V2
 mergedSet <- rbind(trainingSet, testSet)
 
 # Extract the only the measurements on mean and standard deviation for each measurement.
-relevantColumns <- grep("mean|Mean|std", names(mergedSet))
+relevantColumns <- grep("mean|std", names(mergedSet))
 mergedSet <- mergedSet[, relevantColumns]
 
-# Create a column for Activities. We will treat this column as a factor variable with
+# Make the variable names of the merged set more readable by elminating the dashes.
+names(mergedSet) <- gsub('[-()]', '', names(mergedSet))
+
+# Create a column for the activities. We will treat this column as a factor variable with
 # six levels: (1) Walking, (2) Walking Upstairs, (3) Walking Downstairs, (4) Sitting,
 # (5) Standing, and (6) Laying
 trainingActivities <- read.csv(
@@ -39,10 +42,10 @@ testActivities <- read.csv(
     sep = "",
     header = FALSE)
 testActivities <- testActivities$V1
-Activities <- factor(c(trainingActivities, testActivities), 
+Activity <- factor(c(trainingActivities, testActivities), 
                      levels = c("1", "2", "3", "4", "5", "6"),
                      labels = c("Walking", "Walking Upstairs", "Walking Downstairs", "Sitting", "Standing", "Laying"))
-mergedSet <- cbind(Activities, mergedSet)
+mergedSet <- cbind(Activity, mergedSet)
 
 # Create a column for Subject id.
 training_id <- read.csv(
@@ -61,6 +64,6 @@ mergedSet$Subject <- as.factor(mergedSet$Subject)
 
 ## Create a second tidy data set with the average of each variable by subject and activity
 
-Tidy <- aggregate(mergedSet[3:88], by = list(Subject, Activities), FUN = mean, na.rm = TRUE)
+Tidy <- aggregate(mergedSet[3:81], by = list(Subject, Activity), FUN = mean, na.rm = TRUE)
 Tidy <- rename(Tidy, Subject = Group.1, Activity = Group.2)
 write.table(x = Tidy, file = "./tidy.txt", row.names = FALSE)
